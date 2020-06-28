6cc8ecf6db1d90d1d50db8136af78735507368c8
web-devll <ryletd@gmail.com>
2020-06-29 01:28:39 +0300

    working

--- Quick Icon Converter/MainWindow.xaml.cs
+++ Quick Icon Converter/MainWindow.xaml.cs
@@ -2,8 +2,14 @@
 using System.Drawing;
 using System.IO;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace Quick_Icon_Converter
 {
@@ -15,34 +21,26 @@ namespace Quick_Icon_Converter
             this.AllowDrop = true;
         }
         private String[] imageToConvertPath;
+               
+        static int w;
+        static int h;
         public void convert()
         {
             if (imageToConvertPath != null)
             {
                 Bitmap thumb = (Bitmap)Image.FromFile(imageToConvertPath[0]);
-                int w;
-                int h;
-                    w = 16;
-                    h = 16;
-                    thumb = ResizeImage(thumb, w, h); //Указыем размер иконки
+                //    thumb.Width = w; //Указыем размер иконки
                 thumb.MakeTransparent();
-
-                Graphics g = Graphics.FromImage(thumb); // allow drawing to it
-                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic; // make the image shrink nicely by using HighQualityBicubic mode
-                g.DrawImage(thumb, 0, 0, w, h);
-                g.Flush();
-
+                               
                 //Generate save file dialog
                 SaveFileDialog sfd = new SaveFileDialog();
                 //Set dialog filter
                 sfd.Filter = "Icon (*.ico)|*.ico|All files (*.*)|*.*";
-                if (sfd.ShowDialog() == DialogResult.OK)
+                if (sfd.ShowDialog() == true)
                 {
-                    Stream IconStream = System.IO.File.Create(sfd.FileName);
-
-                    Icon icon = ImageHelper.Convert(thumb, new Size(w, h));
-                    this.Icon = icon;
-                    this.Icon.Save(IconStream);
+                    Stream IconStream = File.Create(sfd.FileName);
+                    Icon icon = ImageHelper.Convert(thumb, new System.Drawing.Size(w, h));
+                    icon.Save(IconStream);
                     IconStream.Close();
                 }
             }
@@ -65,46 +63,90 @@ namespace Quick_Icon_Converter
 
         private void Minimized(object sender, RoutedEventArgs e)
         {
+            WindowState = WindowState.Minimized;
         }
-
-        System.Windows.Forms.NotifyIcon nIcon = new System.Windows.Forms.NotifyIcon(); //Иконка для трея
-        void TrayOff(object sender, EventArgs e)
+        void Brows_Img()
         {
-            Show();
-            nIcon.Visible = false;
-
-        }
-        private void Tray_Minimized(object sender, RoutedEventArgs e)
-        {
-            if (nIcon.Visible != true)
-            {
-                nIcon.Icon = new Icon(@"C:\Users\ryletd\Desktop\favicon.ico");
-                nIcon.Visible = true;
-                nIcon.Click += TrayOff;
-                Hide();
-            }
-            else
-                Show();
-        }
-
-        private void Browse(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == true)
+            OpenFileDialog obj = new OpenFileDialog();
+            obj.Multiselect = false;
+            if (obj.ShowDialog() == true)
             {
                 try
                 {
-                    Img.Source = Image.FromFile(ofd.FileName);
+                    Uri fileUri = new Uri(obj.FileName);
+                    Img.Source = new BitmapImage(fileUri);
                 }
                 catch (Exception i)
                 {
-                    System.Windows.MessageBox.Show("Invalid image file. Please select a valid file. Exception: " + i.Message);
+                    MessageBox.Show("Invalid image file. Please select a valid file. Exception: " + i.Message);
                 }
 
-
                 imageToConvertPath = new String[1]; //Must instantiate array to prevent null reference exception
-                imageToConvertPath[0] = ofd.FileName;
+                imageToConvertPath[0] = obj.FileName;
             }
+            S6.IsChecked = true;
+        }
+        private void Browse(object sender, RoutedEventArgs e)
+        {
+            Brows_Img();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            convert();
+        }
+
+        private void Img_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Brows_Img();
+        }
+
+        private void TBack_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BBack_Checked(object sender, RoutedEventArgs e)
+        {  
+
+        }
+
+        private void WBack_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void S6_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 256;
+            h = 256;
+        }
+
+        private void S5_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 128;
+            h = 128;
+        }
+
+        private void S4_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 64;
+            h = 64;
+        }
+
+        private void S3_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 48;
+            h = 48;
+        }
+
+        private void S2_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 32;
+            h = 32;
+        }
+
+        private void S1_Checked(object sender, RoutedEventArgs e)
+        {
+            w = 16;
+            h = 16;
         }
     }
 }
